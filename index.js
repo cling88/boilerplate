62c29c4f29b2534fa8305694d836bd2d63bd6f6e
JOEUNJI <cling88@daum.net>
2020-09-24 22:00:04 +0900

    '시크릿파일'

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+const config = require('./config/key')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cling88:cling88@boilerplate.zyxrr.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+
+app.use(bodyParser.urlencoded({extended: true}));  // application/x-www-form-urlencoded 형식을 분석
+app.use(bodyParser.json());  // json 형식을 분석
+
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -17,6 +24,17 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+app.post('/register', (req, res) => {
+    // 화원가입에 필요한 정보들을 client에서 가져오면 그것들을  DB에 넣어준다
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`);
+    console.log(`Example app listening at ${port}! yayy`);
 })
